07091891d72d433a0663d34fa02725227017b13b
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-09-13 23:19:24 +0100

    Update: Added Tasks Model

---
+++ app/server/models/tasks.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Union
+from datetime import datetime
+
+
+class Tasks(BaseModel):
+   name: str = Field(...)
+   description: str = Field(...)
+   status: str = Field(...)
+   priority: str = Field(...)
+   created_time: Union[datetime, None] = None
+   updated_time: Optional[str]
+
+   class Config:
+      json_schema_extra = {
+         "example": [
+            {
+               "name":"Read a Book",
+               "description":"Read a book on entreprenurship for 2 hours",
+               "status":"completed",
+               "priority":"1",
+               "created_at":"2023-09-10T23:23:35.403+00:00",
+               "updated_at":"2023-09-10T23:23:35.403+00:00"
+            }
+         ]
+      }
+
