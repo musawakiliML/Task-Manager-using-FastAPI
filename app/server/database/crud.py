a38b5d0b4231ac3444c0f05a798f570e583e6ef2
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-09-18 13:42:23 +0100

    Added Create task function

--- app/server/database/crud.py
+++ app/server/database/crud.py
@@ -4,6 +4,13 @@ from app.server.models.tasks import TasksSchema
 # Create the necessary endpoints to handle CRUD operations for tasks (Create, Read, Update, Delete).
 
 # Create task
+async def create_task(task: TasksSchema):
+    try:
+        new_task = await tasks.insert_one(tasks)
+        get_new_task = await tasks.find_one(new_task.inserted_id)
+        return get_new_task
+    except Exception as e:
+        return {"Error_message":str(e)}    
 # Get one task
 # Get all tasks
 # Update one task
